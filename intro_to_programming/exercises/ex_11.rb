94f65aea804826370a877e68171d810f40a0414b
toanhtran <toanh_tran@yahoo.com>
2016-02-03 13:43:16 +0900

    finish exercise chapter

--- intro_to_programming/exercises/ex_11.rb
+++ intro_to_programming/exercises/ex_11.rb
@@ -1,9 +1,3 @@
 # I like DevDocs.io  and ruby-doc.org because the information is easy to search for what you need. 
 
 
-<script language="javascript" type="text/javascript">
-for (var i=0; i <= 3; i++) {
-  msg = (i == 2) ? "Happy Birthday dear Scott" : "Happy Birthday to You!" ;
-  alert(msg);
-}
-</script>
---
+++ intro_to_programming/exercises/ex_16.rb
@@ -0,0 +1,7 @@
+a = ['white snow', 'winter wonderland', 'melting ice',
+     'slippery sidewalk', 'salted roads', 'white trees']
+
+a = a.map { |words| words.split}
+a= a.flatten
+
+puts a
